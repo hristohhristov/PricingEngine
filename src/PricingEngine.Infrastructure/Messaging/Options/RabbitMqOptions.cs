2fa2dd5b11ea4f6a4f9078623cc43102542f5b58
Hristo Hristov <hristo.hristovt@gmail.com>
2026-05-29 21:42:40 +0300

    fix Dockerfile

--- src/PricingEngine.Infrastructure/Messaging/Options/RabbitMqOptions.cs
+++ src/PricingEngine.Infrastructure/Messaging/Options/RabbitMqOptions.cs
@@ -2,7 +2,7 @@ namespace PricingEngine.Infrastructure.Messaging.Options;
 
 public class RabbitMqOptions
 {
-    public string Host        { get; set; } = "rabbitmq";
+    public string Host        { get; set; } = "localhost";
     public string VirtualHost { get; set; } = "/";
     public string Username    { get; set; } = "guest";
     public string Password    { get; set; } = "guest";
